7614652ae916bb071d5ec16466c1877236edd59f
fbocse <fbocse@griddynamics.com>
2024-05-29 10:08:23 +0300

    everything

--- cypress-tests/cypress/support/commands.js
+++ cypress-tests/cypress/support/commands.js
@@ -24,10 +24,12 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', (userName, password) => {
-  cy.get('#username').type(userName);
+Cypress.Commands.add('login', (username, password) => {
+  cy.visit('http://localhost:3000');
+  cy.get('#username').type(username);
   cy.get('#password').type(password);
-  cy.get('[data-test=signin-submit]').click();
+  cy.get('.MuiButton-label').click();
+  cy.url().should('eq', 'http://localhost:3000/');
 });
 
 Cypress.Commands.add('loginRequest', (userName, password) => {
@@ -42,3 +44,40 @@ Cypress.Commands.add('loginRequest', (userName, password) => {
     failOnStatusCode: false,
   });
 });
+
+Cypress.Commands.add('loginByGoogleApi', () => {
+  cy.log('Logging in to Google')
+  cy.request({
+    method: 'POST',
+    url: 'https://www.googleapis.com/oauth2/v4/token',
+    body: {
+      grant_type: 'refresh_token',
+      client_id: Cypress.env('googleClientId'),
+      client_secret: Cypress.env('googleClientSecret'),
+      refresh_token: Cypress.env('googleRefreshToken'),
+    },
+  }).then(({ body }) => {
+    const { access_token, id_token } = body
+
+    cy.request({
+      method: 'GET',
+      url: 'https://www.googleapis.com/oauth2/v3/userinfo',
+      headers: { Authorization: `Bearer ${access_token}` },
+    }).then(({ body }) => {
+      cy.log(body)
+      const userItem = {
+        token: id_token,
+        user: {
+          googleId: body.sub,
+          email: body.email,
+          givenName: body.given_name,
+          familyName: body.family_name,
+          imageUrl: body.picture,
+        },
+      }
+
+      window.localStorage.setItem('googleCypress', JSON.stringify(userItem))
+      cy.visit('/')
+    })
+  })
+})
--- playwright-tests/tests/api/contacts.spec.js
+++ playwright-tests/tests/api/contacts.spec.js
@@ -1,15 +1,62 @@
 const { test, expect } = require('@playwright/test');
-const { testUser } = require('../../data/users');
 const { loggedInUserApiContext } = require('../../utilities/apiUtilities');
+const { testUser, nonExistingUser } = require('../../data/users');
 
 test.beforeAll(async ({ playwright, request }) => {
   await loggedInUserApiContext(playwright, request, testUser);
 });
 
-test('should get contacts', async ({ request }) => {
-  const contactsResponse = await request.get('/transactions/contacts');
-  let data = await contactsResponse.json();
+test("Should register a new account.", async ({ request }) => {
+  const newUser = {
+    username: "FlorinBocse",
+    password: "pass1234"
+  };
+  const registerResponse = await request.post("/users", {
+    data: newUser
+  });
+  expect(registerResponse.status()).toBe(201);
+});
 
-  expect(contactsResponse.ok()).toBeTruthy();
-  expect(data.results).toBeDefined();
+test("Should get list of users.", async ({ request }) => {
+  const getUsersResponse = await request.get("/users");
+  expect(getUsersResponse.ok()).toBeTruthy();
+  const usersList = await getUsersResponse.json();
+  expect(usersList).toBeDefined();
+});
+
+test("Should get user profile by username.", async ({ request }) => {
+  const username = "Tavares_Barrows"; 
+  const getUserProfileResponse = await request.get("/users/profile/${username}");
+  expect(getUserProfileResponse.ok()).toBeTruthy();
+  const responseBody = await getUserProfileResponse.json();
+  expect(typeof responseBody).toBe("object");
+});
+
+test("Should get list of bank accounts.", async ({ request }) => {
+  const response = await request.get("/bankAccounts");
+  expect(response.status()).toBe(200);
+  const bankAccounts = await response.json();
+  expect(bankAccounts.results).toBeDefined();
+  expect(bankAccounts.results.length).toBeGreaterThan(0);
+});
+
+test("Should delete a bank account.", async ({ request }) => {
+  const bankAccountId = "WgiYCFrxjGIo"; 
+  const response = await request.delete(`/bankAccounts/${bankAccountId}`, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  expect(response.status()).toBe(200);
+  const responseData = await response.json();
+  expect(responseData.success).toBeFalsy();
+});
+
+test("Should get notifications list.", async ({ request }) => {
+  const getNotificationsResponse = await request.get("/notifications");
+  expect(getNotificationsResponse.ok()).toBeTruthy();
+  const responseBody = await getNotificationsResponse.json();
+  const notificationsList = responseBody.results;
+  expect(Array.isArray(notificationsList)).toBeTruthy();
+  expect(notificationsList.length).toBeGreaterThan(0);
 });
